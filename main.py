831aa2a966b0e322ef6985262886491c9e7b53b7
mahir-41 <mahiralam41@gmail.com>
2022-07-19 19:39:26 +0600

    Split API into two endpoints

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyxelate import Pyx
 from skimage import io
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 from PIL import Image
 import requests
@@ -11,21 +11,23 @@ app = FastAPI()
 async def test():
     return {"message": "Test successful"}
 
-@app.get("/pixelate")
-async def pixelate(url):
-    img_data = requests.get(url).content
-    with open('image_name.jpg', 'wb') as handler:
-        handler.write(img_data)
-
+@app.get("/pixelate", response_class=FileResponse)
+async def pixelate():
     image = Image.open('image_name.jpg')
     new_image = image.resize((100, 100))
     new_image.save('image_name.png')
 
     image = io.imread("image_name.png") 
 
-    new_image = Pyx(palette=10, dither="bayer", alpha=.6).fit_transform(image)
+    new_image = Pyx(palette=10, dither ="none", alpha=.6).fit_transform(image)
 
     io.imsave("pixel.png", new_image)
 
-    filename = 'pixel.png'
-    return FileResponse(filename)
+    return FileResponse("pixel.png")
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    contents = await file.read()
+    with open('image_name.jpg', 'wb') as handler:
+        handler.write(contents)
+    return {"filename": file.filename}
