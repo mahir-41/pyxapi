0b881660c8388c7b6686a04a7ba2314216336451
mahir-41 <mahiralam41@gmail.com>
2022-07-27 14:55:45 +0600

    Upload feature removed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyxelate import Pyx
 from skimage import io
-from fastapi import FastAPI, File, UploadFile, Response
+from fastapi import FastAPI, File
 from fastapi.responses import FileResponse
 from PIL import Image
 import requests
@@ -8,13 +8,15 @@ import requests
 app = FastAPI()
 
 @app.get("/test")
-async def test(response: Response):
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+async def test():
     return {"message": "Test successful"}
 
 @app.get("/pixelate")
-async def pixelate(response: Response):
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+async def pixelate(url):
+    img_data = requests.get(url).content
+    with open('image_name.jpg', 'wb') as handler:
+        handler.write(img_data)
+
     image = Image.open('image_name.jpg')
     new_image = image.resize((50, 50))
     new_image.save('image_name.png')
@@ -26,11 +28,3 @@ async def pixelate(response: Response):
     io.imsave("pixel.png", new_image)
 
     return FileResponse("pixel.png")
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile, response: Response):
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
-    contents = await file.read()
-    with open('image_name.jpg', 'wb') as handler:
-        handler.write(contents)
-    return {"filename": file.filename}
